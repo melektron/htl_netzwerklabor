f97a9b92807d7d31dc36811f32e6ddef611c6dd7
melektron <matteo@elektron.work>
2023-12-05 12:17:25 +0100

    added better landing pate

--- ex3_sniffing/flomatteo.php
+++ ex3_sniffing/flomatteo.php
@@ -3,10 +3,9 @@
   // To access the data sent with the GET method, you can use $_GET
   $uname = htmlspecialchars($_POST['uname']);
   $psw  = htmlspecialchars($_POST['psw']);
-
+  
+  echo '<html><body>';
   echo  'Username: ', $uname, '</p>', 'Password: ',$psw, '</p>';
-
-
-  echo '<input type="button" onclick="history.back()">Return to login</input>';
+  echo '<button type="submit" onclick="history.back()">Return to login</button>';
+  echo '</body></html>';
 ?>
-
